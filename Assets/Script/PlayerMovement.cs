8f1fb953e1fd7bb53c4554b51df80cf49f279082
cagataydurna <cagataydurna6@gmail.com>
2022-06-15 01:38:11 +0300

    Added FanAnimationController

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public static PlayerMovement _instance;
     public Touch theTouch;
+    public float movementSpeed=10f;
+    public float horSpeed=5f;
+    
 
     private void Awake()
     {
@@ -18,6 +21,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime*10);
+        transform.Translate(Vector3.forward*Time.deltaTime*movementSpeed);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3f, 3f),
+            transform.position.y,
+            transform.position.z);
+        if (Input.touchCount > 0)
+        {
+            theTouch = Input.GetTouch(0);
+            if (theTouch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(
+                    transform.position.x + theTouch.deltaPosition.x * Time.deltaTime * horSpeed,
+                    transform.position.y, transform.position.z);
+            }
+        }
     }
 }
