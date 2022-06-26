d0e5b541f2497fb795372a8366ea776cef48b9ae
arrdakaya <arrdakaya@hotmail.com>
2022-06-27 01:15:33 +0300

    Shop phase2

--- Assets/Script/Shop.cs
+++ Assets/Script/Shop.cs
@@ -22,6 +22,29 @@ public class Shop : MonoBehaviour
     public GameObject lock1;
     public GameObject lock2;
 
+    public void Awake()
+    {
+        if (PlayerPrefs.HasKey("itemSelect") == false)
+            PlayerPrefs.SetInt("itemSelect", 0);
+        if(PlayerPrefs.GetInt("itemSelect") == 0)
+        {
+            Item1Open();
+        }
+
+
+        if (PlayerPrefs.HasKey("controlLock1") == false)
+            PlayerPrefs.SetInt("controlLock1", 0);
+        
+        if (PlayerPrefs.HasKey("controlLock2") == false)
+            PlayerPrefs.SetInt("controlLock2", 0);
+        
+        if (PlayerPrefs.GetInt("controlLock1") == 1)
+            lock1.SetActive(false);
+
+        if (PlayerPrefs.GetInt("controlLock2") == 1)
+            lock2.SetActive(false);
+    }
+
     //ITEMS
     public void Item1Open()
     {
@@ -29,6 +52,7 @@ public class Shop : MonoBehaviour
         ParticleEffect2.SetActive(false);
         item.GetComponent<Image>().sprite = Green;
         item2.GetComponent<Image>().sprite = Yellow;
+        PlayerPrefs.SetInt("itemSelect", 0);
     }
     public void Item2Open()
     {
@@ -43,10 +67,12 @@ public class Shop : MonoBehaviour
     public void Lock1Open()
     {
         int coin = PlayerPrefs.GetInt("coinn");
-        if (coin >= 500)
+        int controlLock1 = PlayerPrefs.GetInt("controlLock1");
+        if (coin >= 500 && controlLock1 == 0)
         {
             lock1.SetActive(false);
             PlayerPrefs.SetInt("coinn", coin - 500);
+            PlayerPrefs.SetInt("controlLock1", 1);
             Item1Open();
             UIManager._instance.CoinUpdate();
         }
@@ -55,10 +81,12 @@ public class Shop : MonoBehaviour
     public void Lock2Open()
     {
         int coin = PlayerPrefs.GetInt("coinn");
-        if (coin >= 1000)
+        int controlLock2 = PlayerPrefs.GetInt("controlLock2");
+        if (coin >= 1000 && controlLock2 == 0)
         {
             lock2.SetActive(false);
             PlayerPrefs.SetInt("coinn", coin - 1000);
+            PlayerPrefs.SetInt("controlLock2", 1);
             Item2Open();
             UIManager._instance.CoinUpdate();
         }
